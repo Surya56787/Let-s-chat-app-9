c0c60bfd9a56280d4d77553f9a1da47e55fd32ac
Surya56787 <105086420+Surya56787@users.noreply.github.com>
2023-04-15 17:48:48 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAf_wY2KAroAeMNDLEOrqGAGnVkd6SU4wc",
+    authDomain: "let-s-chat-app-32856.firebaseapp.com",
+    projectId: "let-s-chat-app-32856",
+    storageBucket: "let-s-chat-app-32856.appspot.com",
+    messagingSenderId: "469754388662",
+    appId: "1:469754388662:web:f18024d686ebe10c8e8617"
+  };
+  
+  initializeApp(firebaseConfig);
